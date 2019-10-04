8cc08ba17a46d17313cc567c25365c812cf1cda1
PournamiJ <pournamij.packt@gmail.com>
2019-10-04 17:05:48 +0530

    Update

--- Chapter08/Example/main.go
+++ Chapter08/Example/main.go
@@ -1,20 +1,14 @@
 package strings
-
 import (
 	"unicode/utf8"
 	"unsafe"
 )
-
-
 type Builder struct {
 	addr *Builder // of receiver, to detect copies by value
 	buf  []byte
 }
-
-
 // https://golang.org/src/strings/compare.go
 package strings
-
 func Compare(a, b string) int {
 	if a == b {
 		return 0
@@ -24,8 +18,6 @@ func Compare(a, b string) int {
 	}
 	return +1
 }
-
-
 // https://golang.org/src/strings/replace.go
 package strings
 
@@ -33,7 +25,6 @@ import (
 	"io"
 	"sync"
 )
-
 type Replacer struct {
 	once   sync.Once // guards buildOnce method
 	r      replacer
